0b3e37257001d5a86ebec89d098b31ccb517102e
Bitaniya Damtew <bitaniyadamtew@gmail.com>
2026-02-12 10:47:05 +0300

    Time: 0 ms (100%), Space: 20.1 MB (29.81%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,8 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        try:
+            nums.index(target)
+        except ValueError:
+            nums.append(target)
+            nums.sort()
+        return nums.index(target)
